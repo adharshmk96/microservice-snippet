5adb97b2ca674bdf826b3c67e7c3c65b09fc22d6
adharshmk96 <dev.adharshmk96@gmail.com>
2020-06-04 22:06:22 +0530

    TS Interface added for user schema

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -2,8 +2,20 @@ import mongoose from 'mongoose';
 
 // An Interface that describes the properties
 // that are required to create a new user
-
 interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+// An Interface that Describes the properties
+// that a user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+// An Interfact that Describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
 	email: string;
 	password: string;
 }
@@ -19,10 +31,10 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
 	return new User(attrs);
 };
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
