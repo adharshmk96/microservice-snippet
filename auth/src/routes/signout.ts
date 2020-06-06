89dab3bb111f5d2e80974f0ec7fd425752e3c8cc
adharshmk96 <dev.adharshmk96@gmail.com>
2020-06-06 23:31:55 +0530

    Added currntuser and require auth as middleware

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { currentUser } from '../middlware/current-user';
+import { requrieAuth } from '../middlware/require-auth';
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-    res.send("Hello There");
+router.post('/api/users/signout', currentUser, requrieAuth, (req, res) => {
+	req.session = null;
+
+	res.send({});
 });
 
 export { router as signoutRouter };
