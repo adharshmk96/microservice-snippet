c88ed52c6f5be9051a89beb46cbb5758bbf0a7a9
adharshmk96 <dev.adharshmk96@gmail.com>
2020-06-06 19:23:46 +0530

    Added request validation middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
 
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlware/validate-request';
 
 const router = express.Router();
 
 router.post(
 	'/api/users/signup',
+	//Middlewares
 	[
 		body('email').isEmail().withMessage('Email must be Valid !'),
 		body('password')
@@ -18,13 +19,9 @@ router.post(
 			.isLength({ min: 4, max: 20 })
 			.withMessage('Password must be between 4 and 20 characters'),
 	],
+	validateRequest,
+	// Request Res Handler
 	async (req: Request, res: Response) => {
-		const errors = validationResult(req);
-
-		if (!errors.isEmpty()) {
-			throw new RequestValidationError(errors.array());
-		}
-
 		// New User ({email,password})
 		const { email, password } = req.body;
 
@@ -47,7 +44,7 @@ router.post(
 		);
 
 		// Store it on session object
-		
+
 		req.session = {
 			// isNew: true,
 			// isChanged: false,
