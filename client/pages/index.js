19253c2831b19ef664a5279ff96ade5fc118af13
adharshmk96 <dev.adharshmk96@gmail.com>
2020-06-10 21:46:15 +0530

    modified common app and passed props down

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,23 @@
-const LandingPage = ({ color }) => {
-    console.log("I'm in Component", color)
-    return <h1>Landing Pages</h1>
+import buildClient from '../api/build-client';
+import axios from 'axios';
+
+
+const LandingPage = ({ currentUser }) => {
+
+    return currentUser ? <h1>Signed in</h1> : <h1>Sign out</h1>
+
+
 }
 
-LandingPage.getInitialProps = () => {
-    console.log("i'm in the server")
-    return { color: "red" }
+LandingPage.getInitialProps = async (context) => {
+    try {
+        const client = buildClient(context)
+        const { data } = await client.get('/api/users/currentuser');
+        return data
+    }
+    catch (err) {
+        console.log(err.message)
+    }
 }
 
 export default LandingPage;
